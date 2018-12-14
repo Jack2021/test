1c2247a1cd38fb84d9037f064beb02d210183a66
Bob <onniale@gmail.com>
2018-12-14 18:12:25 +0100

    resolved some bugs

--- api.js
+++ api.js
@@ -1,12 +1,16 @@
 const express = require('express')
-const bearerToken = require('express-bearer-token');
 const bodyParser = require("body-parser");
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(bearerToken());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World! Visit <a href="/team">/team</a> for team members'));
+
+
+
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT, () => console.log('sqrt listening on port:'+ PORT))
+ }
